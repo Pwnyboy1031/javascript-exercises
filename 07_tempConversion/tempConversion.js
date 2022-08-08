2712b44675c4a7e1d49ab581f7f8edc4a502fbe2
Taylor Todd <ttodd1031@gmail.com>
2022-08-07 23:50:54 -0500

    finished ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(begTemp) {
+  return parseFloat(((begTemp - 32) * (5/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(begTemp) {
+  return parseFloat((begTemp * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
