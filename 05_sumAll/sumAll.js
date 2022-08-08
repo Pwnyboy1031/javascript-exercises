88e522f205542130dae1e35c5aa63066a0fd65ff
Taylor Todd <ttodd1031@gmail.com>
2022-08-07 23:26:49 -0500

    finished ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0){
+        return 'ERROR';
+    }
+
+    if (typeof(first) != "number" || typeof(second) != "number" ) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (first < second) {
+        sum = first;
+    } else {
+        sum = second;
+    }
+    let n = sum+1;
+    let diff = Math.abs(first - second);
+
+    while (diff != 0) {
+        sum += n;
+        n++;
+        diff -= 1;
+    }
+
+    return sum;
 
 };
 
