1c316c64173fe9e7cee3649f334dc4600865b3ca
Taylor Todd <ttodd1031@gmail.com>
2022-08-07 20:38:40 -0500

    Finished ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split("");
+    strArray.reverse();
+    let revStr = strArray.join("");
+    return revStr;
 };
 
 // Do not edit below this line
